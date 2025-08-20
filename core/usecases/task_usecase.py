2b2592fa0db6c78aa6334845b95ba09a86d5d95f
amir ziaee <amir.fr2013@yahoo.com>
2025-08-20 21:23:00 +0330

    create some files for structure

---
+++ core/usecases/task_usecase.py
@@ -0,0 +1,14 @@
+class TaskUseCase:
+    def __init__(self):
+        self.tasks = []
+
+    def create_task(self, task_data):
+        pass
+
+
+    def list_tasks(self):
+        pass
+
+
+    def delete_task(self, task_id):
+        pass
